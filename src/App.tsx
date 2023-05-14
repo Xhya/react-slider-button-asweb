695d5640580946a6a93d0ba0a234f84b79da419c
Alexia Souvane <alexiasouvane@MacBook-Pro-de-Alexia.local>
2023-05-14 15:45:36 +0200

    app init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Slider from "./lib/components/slider/slider.component";
+import NeonImage from "./assets/neon.jpg";
 
 function App() {
+  function onSlide() {}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="app">
+        <Slider
+          content={"Slide pour découvrir"}
+          onSlide={onSlide}
+          image={NeonImage}
+        />
+      </div>
+    </>
   );
 }
 
